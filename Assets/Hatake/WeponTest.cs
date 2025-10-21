f7ea673df1b8e5ab12cfeb9226bf75038cd97098
hatake <katatumuridon@gmail.com>
2025-10-21 19:00:50 +0900

    add wepon

--- Assets/Hatake/WeponTest.cs
+++ Assets/Hatake/WeponTest.cs
@@ -3,23 +3,39 @@ using DG.Tweening;
 
 public class WeponTest : MonoBehaviour
 {
+    private Transform playerTransform;
+
+    private float Radius = 5.0f;
+    private float rotationTime = 5.0f;
+
+    private Tween rotationTween;
+
     void Start()
     {
-        Vector3[] path = {
-            new Vector3(0, 5, 0),
-            new Vector3(5, 0, 0),
-            new Vector3(0, -5, 0),
-            new Vector3(-5, 0, 0),
-            };
-
-        transform.DOLocalPath(path, 5.0f, PathType.CatmullRom)
-            .SetOptions(true);
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+            StartRotation();
+        }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    void StartRotation()
     {
-        
+        rotationTween = DOVirtual.Float(360f, 0f, rotationTime, (angle) =>
+        {
+            if (playerTransform == null) return;
+
+            float rad = angle * Mathf.Deg2Rad;
+
+            float x = Mathf.Cos(rad) * Radius;
+            float y = Mathf.Sin(rad) * Radius;
+
+            UnityEngine.Vector3 targetPosition = playerTransform.position + new UnityEngine.Vector3(x, y, 0);
+
+            transform.position = targetPosition;
+        })
+        .SetEase(Ease.Linear)
+        .SetLoops(-1, LoopType.Restart);
     }
 }
