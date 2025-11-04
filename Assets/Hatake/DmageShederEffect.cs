d4d3468dde5c34ac03c40ccda55108b78c518912
hatake <katatumuridon@gmail.com>
2025-11-04 18:59:10 +0900

    add changecolor
    ダメージを受けた時に赤くさせるシェーダーとスクリプトの追加

---
+++ Assets/Hatake/DmageShederEffect.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using Survivor.Game;
+using UnityEngine;
+
+public class DamageShederEffect : MonoBehaviour
+{
+    [SerializeField] private float effectTime = 0.5f;
+
+    private HealthComponent healthComponent;
+    private Renderer targetRenderer;
+    private Material materialInstance;
+
+    private Coroutine flashCoroutine;
+
+    private static readonly int DamageIntensityProperty = Shader.PropertyToID("_DmageIntensity");
+
+    void Awake()
+    {
+        healthComponent = GetComponent<HealthComponent>();
+        targetRenderer = GetComponentInChildren<Renderer>();
+
+        materialInstance = targetRenderer.material;
+
+        materialInstance.SetFloat(DamageIntensityProperty, 0.0f);
+    }
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+        healthComponent.Damaged += OnDamaged;
+    }
+
+    private void OnDisable()
+    {
+        healthComponent.Damaged -= OnDamaged;
+    }
+
+    private void OnDamaged(float amount, Vector3 worldPos, bool isCrit)
+    {
+        Debug.Log("damage");
+
+        flashCoroutine = StartCoroutine(ResetSpeedAfterDelay());
+    }
+
+    private IEnumerator ResetSpeedAfterDelay()
+    {
+        float timer = 0f;
+
+        materialInstance.SetFloat(DamageIntensityProperty, 1.0f);
+
+        while (timer < effectTime) 
+        {
+            float intensity = 1.0f - ( timer / effectTime );
+            materialInstance.SetFloat(DamageIntensityProperty, intensity);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        materialInstance.SetFloat(DamageIntensityProperty, 0.0f);
+        flashCoroutine = null;
+    }
+}
