3480a14f1c59ac7ab4a647f312961dcdd38c0379
kuneko009 <kuu10682@gmail.com>
2025-11-18 20:01:19 +0900

    bug repair
    同じ武器を選択すると同じアイコンが表示されるバグを直しました

--- Assets/kuneko/Script/WeaponList.cs
+++ Assets/kuneko/Script/WeaponList.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class WeaponList : MonoBehaviour
 {
@@ -11,14 +12,20 @@ public class WeaponList : MonoBehaviour
 
     private GameObject weaponIcon;
     private Image _icon;
+    private List<GameObject> weaponIcons = new List<GameObject>();
 
     private void Start()
     {
         
     }
     public void ShowInWeaponList(Sprite icon)
-    {
+    {   
+        foreach (GameObject weapon in weaponIcons)
+        {
+            if (icon == weapon.GetComponent<Image>().sprite) return;
+        }
         weaponIcon = Instantiate(weaponUIPrefab, root);
+        weaponIcons.Add(weaponIcon);
         _icon = weaponIcon.GetComponent<Image>();
         _icon.sprite = icon;
     }
